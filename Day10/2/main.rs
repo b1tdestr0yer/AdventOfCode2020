fb2f1b91e1b0ec8fd25b5ac4c143e60f18e44f38
andonis1616 <cuzencoandreirobert2008@gmail.com>
2020-12-12 17:53:05 +0200

    Added Day 10 part 2

--- Day10/2/main.rs
+++ Day10/2/main.rs
@@ -8,30 +8,28 @@ fn main() -> Result<(), Error> {
     let mut validOfOne : u32 = 0;
     let mut validOfTwo : u32 = 0;
     let mut validOfThree : u32 = 0;
-    let mut v : Vec<u64> = vec![0];
+    let mut v : Vec<(u64, u64)> = vec![(0,1)];
 
     for line in buffered.lines() {
         let lineAux = line.unwrap();
-        let aux : u64 = lineAux.parse().unwrap();
-        v.push(aux);
+        let jolt : u64 = lineAux.parse().unwrap();
+        v.push((jolt, 0u64));
     }
 
-    v.sort();
-
-    v.push(v[v.len() - 1 ] + 3);
+    v.sort_by_key(|k| k.0);
+    v.push(((v[v.len() - 1 ].0 + 3u64), 0u64));
 
     for i in 0..(v.len() - 1){
-        if v[i + 1] - v[i] == 1{
-            validOfOne = validOfOne + 1;
+        v[i + 1].1 = v[i + 1].1 + v[i].1;
+        if (i + 2) < v.len() && (v[i + 2].0 - v[i].0) <= 3{
+            v[i + 2].1 = v[i + 2].1 + v[i].1;
         }
-        else{
-            if v[i + 1] - v[i] == 3{
-                validOfThree = validOfThree + 1;
-            }
-        }
+        if (i + 3) < v.len() && (v[i + 3].0 - v[i].0) <= 3{
+            v[i + 3].1 = v[i + 3].1 + v[i].1;
+        } 
     }
 
-    println!("{} and {}", validOfOne, validOfThree);
+    println!("{:?}", v[v.len() - 1].1);
 
     return Ok(());
 }
